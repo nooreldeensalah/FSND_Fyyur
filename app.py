6d611699f993ca6c418f794e51524b153bc80bd9
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-06-07 07:24:06 +0200

    Add missing properties and Show model

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 
 # ----------------------------------------------------------------------------#
 # App Config.
@@ -21,44 +22,63 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object("config")
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # ----------------------------------------------------------------------------#
 # Models.
 # ----------------------------------------------------------------------------#
 
+Show = db.Table(
+    "Show",
+    db.MetaData,
+    db.Column("venue_id", db.Integer, db.ForeignKey("Venue.id"), primary_key=True),
+    db.Column("artist_id", db.Integer, db.ForeignKey("Artist.id"), primary_key=True),
+    db.Column("start_time", db.DateTime),
+)
+
 
 class Venue(db.Model):
     __tablename__ = "Venue"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(70))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    genres = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    past_shows = db.Column(db.String(120))
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows = db.Column(db.String(120))
+    upcoming_shows_count = db.Column(db.Integer)
+    artists = db.relationship("Artist", secondary=Show, backref="venues")
 
 
 class Artist(db.Model):
     __tablename__ = "Artist"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(70))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    past_shows = db.Column(db.String(120))
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows = db.Column(db.String(120))
+    upcoming_shows_count = db.Column(db.Integer)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 # ----------------------------------------------------------------------------#
 # Filters.
